d1cb5d1b874eb75d23a7c9fa04d96b7bf128283e
Rafael Lima <rafaa99@gmail.com>
2022-12-08 18:32:11 -0300

    create concat & testing with flow

--- src/flow/flow.ts
+++ src/flow/flow.ts
@@ -6,18 +6,23 @@ import {
   multiply2,
   multiply3,
   convertToString,
-  typeOf
+  typeOf,
+  concat
 } from './helpers';
 
 const result = flow(multiply1)(2); // 2
 const result2 = flow(multiply2, multiply2)(2); //8
 const result3 = flow(multiply3, convertToString)(10); // '30
 
+const tuple = concat(10, flow(multiply2, multiply2, convertToString)); // [10, '40']
+
 console.log({
   result,
   result2,
   result3,
+  tuple,
   typeof_result: typeOf(result),
   typeof_result2: typeOf(result2),
-  typeof_result3: typeOf(result3)
+  typeof_result3: typeOf(result3),
+  typeof_tuple: typeOf(tuple),
 });
--- src/flow/helpers.ts
+++ src/flow/helpers.ts
@@ -1,3 +1,4 @@
+// multiply numbers compose functions
 const multiply = (multiplyNumber: number) => (number: number) =>
   number * multiplyNumber;
 
@@ -5,6 +6,14 @@ export const multiply1 = multiply(1);
 export const multiply2 = multiply(2);
 export const multiply3 = multiply(3);
 
+// convert to string
 export const convertToString = (number: number): string => `${number}`;
 
+// return the data type
 export const typeOf = (value: unknown) => typeof value;
+
+// create tuple
+export const concat = (
+  number: number,
+  convert: (numberToConver: number) => string
+): [number, string] => [number, convert(number)];

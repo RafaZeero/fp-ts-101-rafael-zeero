40e235594199a43191d579b923ec776dc4c80f36
RafaZeero <rafaa99@gmail.com>
2023-01-13 14:46:53 -0300

    test: fold function

--- src/fold/fold.ts
+++ src/fold/fold.ts
@@ -1,6 +1,7 @@
 import { rafa, ray, User } from './helper'
 import * as O from 'fp-ts/Option'
 import { pipe } from 'fp-ts/lib/function'
+import { describe, expect, it } from 'vitest'
 
 const rafaOpt = O.some(rafa)
 const rayOpt = O.some(ray)
@@ -13,10 +14,33 @@ const getPet = (userOpt: O.Option<User>) =>
       return O.fromNullable(x.pet)
     }),
     O.fold(
-      () => console.log('User has no pet!!'),
-      pet => console.log(pet)
+      () => 'User has no pets',
+      pet => `Nice pet : ${pet}`
     )
   )
 
 getPet(rafaOpt)
 getPet(rayOpt)
+
+describe("Get user's pet", () => {
+  it('should not return a pet', () => {
+    const noPetUser = O.some<User>({
+      name: 'fake-user',
+      class: 'mage',
+      level: 10
+    })
+
+    expect(getPet(noPetUser)).toBe('User has no pets')
+  })
+
+  it('should return a pet', () => {
+    const petUser = O.some<User>({
+      name: 'has pet',
+      class: 'mage',
+      level: 15,
+      pet: "I'm a pet"
+    })
+
+    expect(getPet(petUser)).toBe("Nice pet : I'm a pet")
+  })
+})

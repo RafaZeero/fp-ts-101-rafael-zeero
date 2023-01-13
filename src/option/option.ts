5bfc743c134d70e19c9bd04685af83d917d2d832
RafaZeero <rafaa99@gmail.com>
2023-01-13 11:31:42 -0300

    refactor(OPTION): change unknown type to generic A

--- src/option/option.ts
+++ src/option/option.ts
@@ -1,41 +1,39 @@
 // types OPTION construction
 type Some<A> = {
-  _tag: 'Some';
-  value: A;
-};
+  _tag: 'Some'
+  value: A
+}
 
 type None = {
-  _tag: 'None';
-};
+  _tag: 'None'
+}
 
-type Option<A> = Some<A> | None;
+type Option<A> = Some<A> | None
 
 // Examples
 
-const numberZero = 0; // ou Null
+const numberZero = 0 // ou Null
 
 const numberOptionSome: Option<number> = {
   _tag: 'Some',
   value: numberZero
-};
+}
 
 const numberOptionNone: Option<number> = {
   _tag: 'None'
-};
+}
 
 // Functions in tp-ts to use with Option
 
-const isSome = <unkonwn>(option: Option<unkonwn>): option is Some<unkonwn> =>
-  option._tag === 'Some';
-const isNone = <unkonwn>(option: Option<unkonwn>): option is None =>
-  option._tag === 'None';
+const isSome = <A>(option: Option<A>): option is Some<A> => option._tag === 'Some'
+const isNone = <A>(option: Option<A>): option is None => option._tag === 'None'
 
 // Option Some will log the value
-if (isSome(numberOptionSome)) console.log('numberOptionSome Is Some!');
+if (isSome(numberOptionSome)) console.log('numberOptionSome Is Some!')
 // Option None will NOT log the value
-if (isSome(numberOptionNone)) console.log('numberOptionNone Is Some!');
+if (isSome(numberOptionNone)) console.log('numberOptionNone Is Some!')
 
 // Option Some will NOT log the value
-if (isNone(numberOptionSome)) console.log('numberOptionSome Is None!');
+if (isNone(numberOptionSome)) console.log('numberOptionSome Is None!')
 // Option None will log the value
-if (isNone(numberOptionNone)) console.log('numberOptionNone Is None!');
+if (isNone(numberOptionNone)) console.log('numberOptionNone Is None!')

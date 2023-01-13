9d050883441c76acba45cf2355483bb56ab76664
RafaZeero <rafaa99@gmail.com>
2023-01-13 11:35:54 -0300

    refactor(FLOW): make concat helper function more generic instead of just accept type number

--- src/flow/helpers.ts
+++ src/flow/helpers.ts
@@ -1,19 +1,15 @@
 // multiply numbers compose functions
-const multiply = (multiplyNumber: number) => (number: number) =>
-  number * multiplyNumber;
+const multiply = (multiplyNumber: number) => (number: number) => number * multiplyNumber
 
-export const multiply1 = multiply(1);
-export const multiply2 = multiply(2);
-export const multiply3 = multiply(3);
+export const multiply1 = multiply(1)
+export const multiply2 = multiply(2)
+export const multiply3 = multiply(3)
 
 // convert to string
-export const convertToString = (number: number): string => `${number}`;
+export const convertToString = (number: number): string => `${number}`
 
 // return the data type
-export const typeOf = (value: unknown) => typeof value;
+export const typeOf = (value: unknown) => typeof value
 
 // create tuple
-export const concat = (
-  number: number,
-  convert: (numberToConver: number) => string
-): [number, string] => [number, convert(number)];
+export const concat = <A, B>(value: A, convert: (valueToConvert: A) => B): [A, B] => [value, convert(value)]

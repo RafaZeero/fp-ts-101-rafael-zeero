f34733e0fa014e92797e3f0254a95096e3917b0d
RafaZeero <rafaa99@gmail.com>
2023-01-13 11:26:06 -0300

    feat: create server with fp-ts

--- src/express/server.ts
+++ src/express/server.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response } from 'express'
 import * as dotenv from 'dotenv'
+import { pipe } from 'fp-ts/function'
+import * as TE from 'fp-ts/TaskEither'
+import * as E from 'fp-ts/Either'
+import * as O from 'fp-ts/Option'
 dotenv.config()
 
 const app = express()
@@ -9,9 +13,25 @@ app.use(express.urlencoded({ extended: true })) /** Allow req.body */
 app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
-  res.status(200).send('Henlo!!')
+  res.status(200).send('henlo !!')
+})
+
+app.post('/', (req: Request, res: Response) => {
+  const user = O.fromNullable<User>(req.body.user)
+
+  const response = pipe(
+    user,
+    O.map(user => user)
+  )
+
+  O.isSome(response) ? res.send(response.value) : res.send('not valid')
 })
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+type User = {
+  name: string
+  age: number
+}

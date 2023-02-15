762930916977156bcbb64ceb6e2495f5276eca51
RafaZeero <rafaa99@gmail.com>
2023-02-15 09:19:31 -0300

    feat: add filter

--- src/array/array.ts
+++ src/array/array.ts
@@ -12,8 +12,12 @@ const array01 = () =>
     log('(FROM ARRAY01) : '),
   )
 
-const foo = [1, 2, 3]
+const foo = [1, 2, 3, 1, 2, 3, 1, 2, 3]
 const bar = ['a', 'b', 'c']
+const foobar = [
+  { name: 'rafa', age: 12 },
+  { name: 'dandan', age: 15 },
+]
 
 const array02 = () => pipe(foo, A.zip(bar), log('(FROM ARRAY02) Zip: '))
 
@@ -29,13 +33,20 @@ const array03 = () =>
     log('(FROM ARRAY03) Lookup: '),
   )
 
-const array04 = () => pipe(foo, A.head, log('from head')) // returns an option
+const array04 = () => pipe(foo, A.head, log('[array 04] from head')) // returns an option
 const array05 = () => {
-  if (A.isNonEmpty(foo)) return pipe(foo, NEA.head, log('from head')) // returns the value itself
+  if (A.isNonEmpty(foo)) return pipe(foo, NEA.head, log('[array 05] from head')) // returns the value itself
 }
+
+const array06 = () =>
+  pipe(
+    A.array.filter(foobar, n => n.age > 14),
+    log('[array 06] from filter: '),
+  )
 
 // array01()
 // array02()
 // array03'()
 array04()
 array05()
+array06()

6a205d6f94d01791136eeadff69b1ef394aec897
RafaZeero <rafaa99@gmail.com>
2023-02-15 09:04:28 -0300

    feat: array lookup test

--- src/array/array.ts
+++ src/array/array.ts
@@ -1,6 +1,7 @@
 import * as A from 'fp-ts/Array'
 import { pipe } from 'fp-ts/lib/function'
 import { log } from './helper'
+import * as E from 'fp-ts/Either'
 
 const array01 = () =>
   pipe(
@@ -10,11 +11,22 @@ const array01 = () =>
     log('(FROM ARRAY01) : '),
   )
 
-array01()
-
 const foo = [1, 2, 3]
 const bar = ['a', 'b', 'c']
 
 const array02 = () => pipe(foo, A.zip(bar), log('(FROM ARRAY02) Zip: '))
 
-array02()
+const array03 = () =>
+  pipe(
+    foo,
+    A.lookup(2),
+    E.fromOption(() => 'error!'),
+    E.fold(
+      err => err,
+      x => x.toString(),
+    ),
+    log('(FROM ARRAY03) Lookup: '),
+  )
+// array01()
+// array02()
+array03()

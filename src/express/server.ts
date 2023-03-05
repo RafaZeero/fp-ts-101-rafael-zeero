6200c999ac8c85ca4d3219cd91d8d905c9048b94
RafaZeero <rafaa99@gmail.com>
2023-03-05 19:44:26 -0300

    refactor: put log into specific folder

--- src/express/server.ts
+++ src/express/server.ts
@@ -1,71 +1,26 @@
-import express, { Request, Response } from 'express'
-import * as dotenv from 'dotenv'
-import { pipe } from 'fp-ts/function'
-import * as E from 'fp-ts/Either'
-import * as O from 'fp-ts/Option'
-import pino from 'pino'
-import { pinoHttp } from 'pino-http'
-import PinoPretty from 'pino-pretty'
-dotenv.config()
+import * as dotenv from 'dotenv';
+import express, { Request, Response } from 'express';
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
+import * as O from 'fp-ts/Option';
+dotenv.config();
 
-const app = express()
-const PORT = process.env['PORT'] || 3333
+const app = express();
+const PORT = process.env['PORT'] || 3333;
 
-const users: User[] = []
+const users: User[] = [];
 
 const user = {
   login: 'rafa',
   senha: '123',
-}
+};
 
-const levels = {
-  emerg: 80,
-  fatal: 70,
-  crit: 60,
-  error: 50,
-  warn: 40,
-  notice: 30,
-  info: 20,
-  debug: 10,
-}
-
-const destination = [
-  { stream: PinoPretty() },
-  { stream: pino.destination('src/express/custom.log') },
-]
-
-export const logger = pino(
-  {
-    name: 'testing-pino',
-    base: { pid: undefined },
-  },
-  pino.multistream(destination),
-)
-
-const pinoMiddleware = pinoHttp(
-  {
-    // customReceivedMessage: () => 'received',
-    customLevels: levels,
-    useOnlyCustomLevels: true,
-  },
-  pino.multistream(
-    [
-      { stream: PinoPretty() },
-      { stream: pino.destination('src/express/custom.log'), level: 'info' },
-      { stream: pino.destination('src/express/custom2.log'), level: 'info' },
-    ],
-    { levels },
-  ),
-)
-
-app.use(express.urlencoded({ extended: true })) /** Allow req.body */
-app.use(express.json())
-app.use(pinoMiddleware)
+app.use(express.urlencoded({ extended: true })); /** Allow req.body */
+app.use(express.json());
 
 app.get('/log', (req: Request, res: Response) => {
-  logger.info('connected')
-  res.send('noice')
-})
+  res.send('noice');
+});
 
 app.get('/', (req: Request, res: Response) => {
   pipe(
@@ -73,8 +28,8 @@ app.get('/', (req: Request, res: Response) => {
     E.fromOption(() => JSON.stringify({ error: 'Unauthorized. Invalid credentials' })),
     E.map(() => res.status(200).send(user)),
     E.mapLeft(error => res.status(401).send(error)),
-  )
-})
+  );
+});
 
 app.post('/login', (req: Request, res: Response) =>
   pipe(
@@ -88,26 +43,26 @@ app.post('/login', (req: Request, res: Response) =>
     E.map(() => res.send('logged in')),
     E.mapLeft(error => res.send(error)),
   ),
-)
+);
 
 app.post('/', (req: Request, res: Response) => {
-  const user = O.fromNullable<User>(req.body.user)
+  const user = O.fromNullable<User>(req.body.user);
 
   const response = pipe(
     user,
     O.map(user => user),
-  )
+  );
 
   O.isSome(response)
     ? users.push(response.value) && res.send(response.value)
-    : res.send('not valid')
-})
+    : res.send('not valid');
+});
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
 
 type User = {
-  name: string
-  age: number
-}
+  name: string;
+  age: number;
+};

0247a3735ee52ef0dfa3f9b0450357a5cd9918b8
RafaZeero <rafaa99@gmail.com>
2023-03-25 12:32:31 -0300

    feat: create toSlug fn with fp-ts

--- src/functional/ramda.ts
+++ src/functional/ramda.ts
@@ -1,4 +1,5 @@
 import { pipe } from 'fp-ts/lib/function';
+import * as A from 'fp-ts/Array';
 import * as R from 'ramda';
 
 type ToSlug = (word: string) => string;
@@ -13,7 +14,21 @@ const toSlug: ToSlug = input => {
 
 const ramdaToSlug: ToSlug = input => R.pipe(R.split(' '), R.map(R.toLower), R.join(''))(input);
 
+const splitFN =
+  (separator: string) =>
+  (input: string): Array<string> =>
+    input.split(separator);
+const lowerFN = (input: string): string => input.toLowerCase();
+const joinFN =
+  (separator: string) =>
+  (input: Array<string>): string =>
+    input.join(separator);
+
+const fptsToSlug: ToSlug = input =>
+  pipe(input, A.of, A.chain(splitFN(' ')), A.map(lowerFN), x => joinFN(' ')(x));
+
 // const slug = toSlug('This is a component');
-const slug = ramdaToSlug('This is a component');
+// const slug = ramdaToSlug('This is a component');
+const slug = fptsToSlug('This is a component');
 
 console.log('slug: ', slug);

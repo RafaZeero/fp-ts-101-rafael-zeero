339197e5b1a2d7b16648287ad0a2e74a52f46d78
RafaZeero <rafaa99@gmail.com>
2023-03-25 11:26:59 -0300

    feat: create toSlug fn

---
+++ src/functional/ramda.ts
@@ -0,0 +1,16 @@
+import { pipe } from 'fp-ts/lib/function';
+import * as R from 'ramda';
+
+type ToSlug = (word: string) => string;
+
+const toSlug: ToSlug = input => {
+  const words = input.split(' ');
+  const lowercasedWords = words.map(word => word.toLowerCase());
+  const slug = lowercasedWords.join('-');
+  const encodedURI = encodeURIComponent(slug);
+  return encodedURI;
+};
+
+const slug = toSlug('This is a component');
+
+console.log('slug: ', slug);

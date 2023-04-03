501095ac1d978dad5fd9ad23f95ef98a580f0680
RafaZeero <rafaa99@gmail.com>
2023-04-03 14:45:57 -0300

    feat: type with me series playing with Option - episode 02

---
+++ typewithme/part2/index.ts
@@ -0,0 +1,30 @@
+import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/lib/function';
+
+type Foo = { bar: string };
+type Fizz = { buzz?: Buzz };
+type Buzz = { fizz: string };
+
+const foo = { bar: 'henlo' } as Foo | undefined;
+
+const fizz = { buzz: undefined } as Fizz;
+
+pipe(
+  foo,
+  O.fromNullable,
+  O.map(({ bar }: Foo) => bar),
+  console.log
+);
+
+pipe(
+  fizz,
+  O.fromNullable,
+  O.map(({ buzz }) =>
+    pipe(
+      buzz,
+      O.fromNullable,
+      O.map(({ fizz }) => fizz)
+    )
+  ),
+  console.log
+);

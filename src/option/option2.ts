fd58834a386261f298f497376b3e24b4b3ff2a50
RafaZeero <rafaa99@gmail.com>
2023-04-05 22:21:59 -0300

    feat: update option2

--- src/option/option2.ts
+++ src/option/option2.ts
@@ -1,8 +1,20 @@
 import * as O from 'fp-ts/Option';
 import { pipe, constNull, identity } from 'fp-ts/lib/function';
-import { BehaviorSubject, map } from 'rxjs';
+import {
+  BehaviorSubject,
+  Subject,
+  filter,
+  interval,
+  map,
+  merge,
+  mergeWith,
+  of,
+  startWith,
+  takeUntil,
+  timer
+} from 'rxjs';
 
-const _example$ = new BehaviorSubject<number>(0);
+const _example$ = new Subject<number>();
 const example$ = _example$.asObservable();
 
 const data$ = example$.pipe(
@@ -11,4 +23,14 @@ const data$ = example$.pipe(
   map(O.fold(constNull, identity))
 );
 
-data$.subscribe(console.log);
+const data2$ = interval(100).pipe(
+  mergeWith(example$),
+  map(O.fromNullable),
+  // map(O.map(x => x * 2)),
+  // filter(x => O.isSome(x) && x.value % 2 === 0), // find a better solution to get only the pairs
+  map(O.fold(constNull, identity)),
+  takeUntil(timer(1000))
+);
+
+// data$.subscribe(console.log);
+data2$.subscribe(console.log);
